b12dcdcd4001361f7271415b5056b6eedf179f11
Schizophrenia <155841631+Narcolepsyy@users.noreply.github.com>
2024-07-24 23:26:08 +0700

    Add files via upload

---
+++ config/options.lua
@@ -0,0 +1,10 @@
+vim.opt.title = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.wrap = true
+
+vim.cmd([[let &t_Cs = "\e[4:3m"]])
+vim.cmd([[let &t_Ce = "\e[4:0m"]])
